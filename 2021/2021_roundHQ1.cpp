e0a136ad9b70b47ce1c5ac035ec12cf7cdf0fb7b
Aansh2003 <96300383+Aansh2003@users.noreply.github.com>
2022-07-13 01:36:36 +0530

    Update 2021_roundHQ1.cpp

--- 2021/2021_roundHQ1.cpp
+++ 2021/2021_roundHQ1.cpp
@@ -1,22 +1,4 @@
 
-/*
-You are given a string S which denotes a padlock consisting of lower case English letters. You are also given a string F consisting of set of favorite lower case English letters.
-You are allowed to perform several operations on the padlock. In each operation, you can change one letter of the string to the one following it or preceding it in the alphabetical order.
-For example: for the letter c, you are allowed to change it to either b or d in an operation. The letters can be considered in a cyclic order, i.e.
-,the preceding letter for letter a would be letter z. Similarly, the following letter for letter z would be letter a.
-Your aim is to find the minimum number of operations that are required such that each letter in string S after applying the operations, is present in string F.
-INPUT EXAMPLE-
-
-2
-abcd
-a
-pqrs
-afg
-
-EXPECTED OUTPUT-
-Case #1 - 6
-Case #2 - 42
-*/
 #include <iostream>
 #include <cstring>
 using namespace std;
