97eb078113b9d22ca7d9ae210df851d8c9ba2204
Aansh2003 <96300383+Aansh2003@users.noreply.github.com>
2022-07-13 01:28:12 +0530

    Update 2018_roundHQ1.cpp

--- 2018/2018_roundHQ1.cpp
+++ 2018/2018_roundHQ1.cpp
@@ -1,29 +1,3 @@
-/*
- You are a contestant on a popular new game show and are playing for the grand prize!
-
-There are two big buttons, a red one and a black one. You will make a sequence of exactly N button presses.
-
-There are lots of different sequences of presses you could make, but there are P forbidden prefixes, each of length no greater than N. If you make a sequence of presses which begins with any of the forbidden sequences, you will not win the grand prize. It is fine for your sequence to contain one or more forbidden prefixes as long as they do not appear at the start of your sequence.
-
-A winning sequence must consist of exactly N button presses and must not begin with one of the forbidden prefixes. How many different winning sequences are there? 
-
-Sample input:
-3
-3 2
-BBB
-RB
-5 1
-R
-4 3
-R
-B
-RBRB
-
-Expected output:
-Case #1: 5
-Case #2: 16
-Case #3: 0
-*/
 
 
 #include <iostream>
